fd086dc6d1bbce6ac6656f54f90d575661f25103
Nivas Malligesan <nivasmalligesan23@gmail.com>
2025-01-31 11:42:10 +0530

    Update bubble Sort.c

--- bubble Sort.c
+++ bubble Sort.c
@@ -1,50 +1,26 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-
-void bubblesort(int *array,int size){
-    bool swapped = 1;
-    int len = size;
-    
-    for(int j = 0 ; j < len ; j ++){
-        swapped =0 ;
-        for(int i = 0 ; i < len-1 ; i++){
-        if(array[i] > array[i+1]){
-            int temp = array[i];
-            array[i] = array[i+1];
-            array[i+1] = temp;
-            swapped = 1 ;
-        }
-    if(swapped == 0)break;
-        }
-    }
-    printf("\n");
-    for(int i = 0 ; i < len ; i ++){
-        printf("%d ",array[i]);
-    }
-}
-
-
 
 int main() {
-    int size = 0;
-    
-    printf("Enter thye number of Size : ");
-    scanf("%d",&size);
-    
-    int* array = (int*)malloc(size*sizeof(int));
-    
-    printf("Enter the Numbers ");
-    for(int i = 0 ; i < size ; i ++){
-        scanf("%d",&array[i]);
+    int a[] = {5,4,2,3,1};
+    int size = 5;
+    for (int steps = 0 ; steps < size-1 ; i ++){
+        int swapped = 0;
+        for(int i = 0 ; i < size - steps - 1 ; i++){
+            if(a[i] > a[i+1]){
+                int temp = a[i];
+                a[i+1] = a[i];
+                a[i] = temp;
+                swapped =1 ;
+            }
+        }
+        if(swapped == 0){
+            break;
+        }
     }
     for(int i = 0 ; i < size ; i ++){
-        printf("%d ",array[i]);
+        printf("%d ",a[i]);
     }
     
-    bubblesort(array, size);
-
-  return 0;
+    
+    return 0;
 }
